eb8e80e8fba7e3943cc96465c5a6aa6b60ef65b6
Barrette Banner <barrettebanner@gmail.com>
2019-08-05 01:09:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -5,7 +5,7 @@ def help
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
-  puts: "Please enter a command:"
+  puts "Please enter a command:"
 end
 
 def list(songs)
